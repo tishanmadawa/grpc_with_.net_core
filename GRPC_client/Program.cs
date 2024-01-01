6607abdde788b3d5bb467039245dc1e93fab021e
TishanML <tishan@creativesoftware.com>
2024-01-01 07:46:12 +0530

    GRPC client implementation

---
+++ GRPC_client/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GRPC_Client;
+
+var data = new HelloRequest { Name = "Joydip" };
+var grpcChannel = GrpcChannel.ForAddress("https://localhost:7107");
+var client = new Greeter.GreeterClient(grpcChannel);
+var response = await client.SayHelloAsync(data);
+Console.WriteLine(response.Message);
+Console.ReadLine();
+
